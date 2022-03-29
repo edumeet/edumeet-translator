b9451eb15ac53c3cea56a415ede257e21f1edfca
stefkamarton <stefkamarton14@gmail.com>
2022-03-29 12:17:03 +0200

    add version

--- html/footer.php
+++ html/footer.php
@@ -1,6 +1,8 @@
 <!-- FOOTER SECTION START -->
 <footer>
-
+    <div class="row">
+        <div class="offset-6 offset-md-9 col-6 col-md-3 text-end"><span class="text-muted fs-6"><strong class="me-2">Version:</strong>1.0.0 beta</span></div>
+    </div>
 </footer>
 <!-- FOOTER SECTION END -->
 <div class="alert-notifications">
